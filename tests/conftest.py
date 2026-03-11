56ee1d6b629728a6fd33f5de85b423b9570073a1
rorybramley <rjbramley95@googlemail.com>
2026-03-11 22:05:21 +0000

    Modified fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
 import pytest
 from pyspark.sql import SparkSession
 
+
 @pytest.fixture(scope="session")
 def spark():
-    spark = (
+    active_spark = SparkSession.getActiveSession()
+    if active_spark is not None:
+        yield active_spark
+        return
+
+    local_spark = (
         SparkSession.builder
         .master("local[2]")
         .appName("turbine-pipeline-tests")
         .getOrCreate()
     )
-    yield spark
-    spark.stop()
+    yield local_spark
+    local_spark.stop()
